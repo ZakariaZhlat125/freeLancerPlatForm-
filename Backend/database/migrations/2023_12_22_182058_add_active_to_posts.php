45331d9923284a59ab4f4d0107ba8b12ea930cdd
ZakariaZhlat125 <zakooorr1567@gmail.com>
2023-12-24 23:04:58 +0300

    add specializations  migration

--- Backend/database/migrations/2023_12_22_182058_add_active_to_posts.php
+++ Backend/database/migrations/2023_12_22_182058_add_active_to_posts.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::table('posts', function (Blueprint $table) {
             //
+            $table->boolean('is_active')->default(1);
         });
     }
 
