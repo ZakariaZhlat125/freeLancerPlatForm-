f5f24037a1b8098c6b80a2898a901dcb9998581d
ZakariaZhlat125 <zakooorr1567@gmail.com>
2023-12-24 21:51:30 +0300

    add laratrust_setup_tables

--- Backend/database/migrations/2023_12_22_181523_laratrust_setup_tables.php
+++ Backend/database/migrations/2023_12_22_181523_laratrust_setup_tables.php
@@ -11,7 +11,59 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+
+
+        //create table for storign roles
+        Schema::create('roles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->unique();
+            $table->string('display_name')->nullable();
+            $table->string('description')->nullable();
+            $table->timestamps();
+        });
+        //  create table for storing permissions
+        Schema::create('permissions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->unique();
+            $table->string('display_name')->nullable();
+            $table->string('description')->nullable();
+            $table->timestamps();
+        });
+        // create table for associating roles to users and teams (Many To Many Polymorphic)
+        Schema::create('role_user', function (Blueprint $table) {
+            $table->unsignedBigInteger('role_id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('user_type');
+            $table->foreign('role_id')->references('id')->on('roles')
+                ->onUpdate('cascade')->onDelete('cascade');
+            $table->primary(['user_id', 'role_id', 'user_type']);
+        });
+
+        // Create table for associating permissions to users (Many To Many Polymorphic)
+
+        Schema::create('permission_user', function (Blueprint $table) {
+            $table->unsignedBigInteger('permission_id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('user_type');
+
+            $table->foreign('permission_id')->references('id')->on('permissions')
+                ->onUpdate('cascade')->onDelete('cascade');
+
+            $table->primary(['user_id', 'permission_id', 'user_type']);
+        });
+
+        // Create table for associating permissions to roles (Many-to-Many)
+        Schema::create('permission_role', function (Blueprint $table) {
+            $table->unsignedBigInteger('permission_id');
+            $table->unsignedBigInteger('role_id');
+
+            $table->foreign('permission_id')->references('id')->on('permissions')
+                ->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('role_id')->references('id')->on('roles')
+                ->onUpdate('cascade')->onDelete('cascade');
+
+            $table->primary(['permission_id', 'role_id']);
+        });
     }
 
     /**
@@ -19,6 +71,10 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('permission_user');
+        Schema::dropIfExists('permission_role');
+        Schema::dropIfExists('permissions');
+        Schema::dropIfExists('role_user');
+        Schema::dropIfExists('roles');
     }
 };
