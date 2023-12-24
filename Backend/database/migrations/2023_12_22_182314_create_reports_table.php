d01b047850e8da68eb047ddd84972367985a75b2
ZakariaZhlat125 <zakooorr1567@gmail.com>
2023-12-24 23:18:26 +0300

    add  reports  migration

--- Backend/database/migrations/2023_12_22_182314_create_reports_table.php
+++ Backend/database/migrations/2023_12_22_182314_create_reports_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('reports', function (Blueprint $table) {
             $table->id();
+            $table->string('type_report')->nullable();
+            $table->string('massege')->nullable();
+            $table->string('provider_id')->nullable();
+            $table->boolean("is_active")->default(1);
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->unsignedBigInteger('post_id')->nullable();
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
             $table->timestamps();
         });
     }
