229144db1dc3277d3bf4a42bf5f95df9f98c6e1c
ZakariaZhlat125 <zakooorr1567@gmail.com>
2023-12-24 21:55:20 +0300

    add skilles and categories

--- Backend/database/migrations/2023_12_22_181610_create_categories_table.php
+++ Backend/database/migrations/2023_12_22_181610_create_categories_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->boolean("is_active")->default(1);
             $table->timestamps();
         });
     }
